5b338eace80425cee065a91e1bf5425e65d698a2
yago2525 <134134003+yago2525@users.noreply.github.com>
2024-03-29 17:06:07 +0500

    19 dars qo'shildi

---
+++ 19_dars.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 15:29:29 2024
+
+@author: user
+"""
+# def t_yil(ism, yosh, joriy_yil=2024):
+#     print(f"{ism.title()} {joriy_yil-yosh}-yilda tug'ilgan")
+    
+# t_yil('sardor', 24)
+
+# son=int(input('son kiriting: '))
+# def kv_kub(son):
+#     print(f"{son} ning kvadrati {son**2} ga teng\n"
+#           f"{son} ning kubi {son**3} ga teng")
+# kv_kub(son)
+
+
+# def j_t(son):
+#     if son%2==0:
+#         print(f"{son} soni juft son")
+#     else:
+#         print(f"{son} soni toq son")
+# j_t(23)
+        
+# def taqqosla(son1, son2):
+#     if son1>son2:
+#         print(son1)
+#     elif son1==son2:
+#         print("sonlar teng")
+#     else:
+#         print(son2)
+# taqqosla(25, 45)
+         
+# def taqqosla(son1, son2=2):
+#     if son1>son2:
+#         print(son1)
+#     elif son1==son2:
+#         print("sonlar teng")
+#     else:
+#         print(son2)
+# taqqosla(25)         
+
+# def bolinish(son):
+#     for i in range(2,10):
+#         if son%i==0:
+#             print(f"{son} soni {i} ga bo'linadi")
+# bolinish(56)
+
